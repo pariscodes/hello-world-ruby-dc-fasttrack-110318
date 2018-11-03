8980accaa2edeacdb8555a73fdd0c8405f5382b6
Paris Henderson <hireparish@gmail.com>
2018-11-03 15:53:38 +0000

    automatically backed up by learn

---
+++ arrays.rb
@@ -0,0 +1,9 @@
+student1 = 25
+student2 = 32
+student3 = 35 
+
+puts student1
+puts student2
+puts student3
+
+students = [25, 32, 35]
---
+++ methods.rb
@@ -0,0 +1,8 @@
+def printHello  
+  puts "hello world"
+end
+
+printHello
+
+puts "hello world"
+puts "welcome to fasttrack"
